42e318197b96e2bbb6b0933f1f3a9972f7fdd6df
Yansen Tjandra <yansentjandra@Yansens-MBP.lan>
2019-01-31 20:15:13 -0500

    fix searchJob function

--- client/public/Job.js
+++ client/public/Job.js
@@ -1,3 +1,18 @@
 function searchJob() {
-  alert("Test");
+  var keyword = document.getElementById("keyword").value;
+  var location = document.getElementById("location").value;
+
+  // alert(keyword);
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8000/input/",
+    dataType: "json",
+    data: {
+      keyword: keyword,
+      location: location,
+    },
+    success: function(result){
+    }
+  });
+
 }
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const Jobs = './models/Jobs';
 
+var input;
+
 const connection = mysql.createConnection ({
   host: 'localhost',
   user: 'root',
@@ -28,11 +30,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(express.static(path.join(__dirname, 'modules')));
+app.use(express.static(path.join(__dirname, '/client/public')));
 
 app.get('/', (req,res) => {
   res.status(200).sendFile(__dirname + '/client/public/index.html');
-
 });
 
 app.get('/api/result', (req, res) => {
@@ -81,7 +82,49 @@ app.post('/api/search', (req, res) => {
   });
 });
 
-app.get('/search', (req, res) => {
+app.get('/api/search', (req, res) => {
+  let keyword = input.keyword;
+  let location = input.location;
+  let queryString = "";
+
+  if (keyword == "" && location =="") {
+    queryString = "SELECT * FROM Job";
+  }
+  else if (keyword == "") {
+    // keyword = "*"
+    queryString = "SELECT * FROM Job WHERE location = '" + location +"'";
+  }
+  else if (location == "") {
+    // location = "*"
+    queryString = "SELECT * FROM Job WHERE name = '" + keyword + "'";
+  }else {
+    queryString = "SELECT * FROM Job WHERE name = '" + keyword + "' AND location = '" + location +"'";
+  }
+
+  // console.log(queryString);
+  connection.query(queryString, (err, result, fields) => {
+    if(err) throw err;
+    // console.log(res);
+    // console.log(fields);
+    res.send(result)
+    // res.redirect('/client/public/job.html');
+    // console.log(result)
+    // show(result, res);
+    // res.sendFile(path.join(__dirname + '/client/public/job.html'));
+
+  });
+});
+
+app.post('/input', (req, res) => {
+  input = req.body;
+});
+
+app.get('/input', (req, res) => {
+  res.send(input);
+  console.log(input);
+});
+
+app.get('/jobs', (req, res) => {
   res.status(200).sendFile(__dirname + '/client/public/job.html');
 });
 
